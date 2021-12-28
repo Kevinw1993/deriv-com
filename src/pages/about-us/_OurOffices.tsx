ebe8f61af027ee229bf49ee001e2ee070a5b3158
Kevin <kevin@regentmarkets.com>
2021-12-28 14:06:10 +0800

    our office map v1

--- src/pages/about-us/_OurOffices.tsx
+++ src/pages/about-us/_OurOffices.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import { graphql, useStaticQuery } from 'gatsby'
-import { localize } from 'components/localization'
+import { localize, LocalizedLink } from 'components/localization'
 import { SectionContainer, CssGrid, Show, Flex } from 'components/containers'
-import { Header, QueryImage, ImageWrapper, Text } from 'components/elements'
+import { Header, QueryImage, ImageWrapper, Text, BackgroundImage } from 'components/elements'
 import device from 'themes/device'
+
+import { ReactComponent as PinPin } from 'images/svg/about-us/pin.svg'
+import { ReactComponent as Chevron } from 'images/svg/custom/chevron-thick.svg'
 
 const query = graphql`
     query {
@@ -29,6 +32,8 @@ const StyledSectionContainer = styled(SectionContainer)`
 `
 
 const StyledHeader = styled(Header)`
+    line-height: 4rem;
+    margin-bottom: 69px;
     @media ${device.laptop} {
         font-size: 28px;
     }
@@ -63,7 +68,7 @@ const NumberSection = styled(CssGrid)`
 const NumberHeader = styled(Text)`
     font-weight: bold;
     line-height: 6rem;
-    text-align: left;
+    text-align: center;
 
     @media ${device.mobileL} {
         margin-bottom: 8px;
@@ -72,48 +77,209 @@ const NumberHeader = styled(Text)`
 
 const NumberText = styled(Text)`
     font-weight: 400;
-    text-align: left;
-    margin-top: 8px;
+    text-align: center;
 `
 
-const StyledQueryImage = styled(QueryImage)`
-    z-index: 2;
+const PinContent = styled(LocalizedLink)`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: var(--color-white);
+    box-shadow: rgba(0, 0, 0, 0.05) 0 16px 20px 0, rgba(0, 0, 0, 0.05) 0 0 20px 0;
+    border-radius: 5px;
+    padding: 1rem 1.6rem;
+    text-align: center;
+    position: relative;
+    left: -43%;
+    top: -55px;
+    transition: opacity 0.25s;
+    z-index: 3;
+    text-decoration: none;
+
+    &::after {
+        content: '';
+        width: 12px;
+        height: 12px;
+        background: var(--color-white);
+        position: absolute;
+        transform: rotate(45deg);
+        top: 83%;
+        left: 40.5%;
+    }
 `
+
+const ChevronRight = styled(Chevron)`
+    transform: rotate(90deg);
+    width: 16px;
+    height: 16px;
+
+    .chevron-thick-path {
+        fill: var(--color-red);
+    }
+`
+
+type PinWrapperProps = {
+    left: string
+    top: string
+    to: string
+}
+
+const PinWrapper = styled.div<PinWrapperProps>`
+    position: absolute;
+    top: ${(props) => props.top};
+    left: ${(props) => props.left};
+    cursor: pointer;
+`
+
+const MapImage = styled(BackgroundImage)`
+    position: relative;
+    width: 840px;
+    overflow: auto;
+    height: 414px;
+    background-color: transparent;
+    background-size: cover;
+
+    /* @media screen and (min-width: 992px) {
+        min-width: 1177px;
+        overflow: hidden;
+    }
+
+    @media screen and (max-width: 992px) and (min-width: 575px) {
+        width: 100%;
+        height: calc(100vw / 2.6);
+    }
+
+    @media screen and (max-width: 575px) {
+        width: 100%;
+        height: calc(100vw / 1.84);
+    }
+
+    @media ${device.tabletS} {
+        margin-bottom: 25px;
+    } */
+`
+
+const MapPin = ({ top, left, title, link }) => {
+    const [is_pin_show, setPinShow] = React.useState(false)
+    return (
+        <PinWrapper
+            onMouseEnter={() => setPinShow(true)}
+            onMouseLeave={() => setPinShow(false)}
+            top={top}
+            left={left}
+            to={link}
+        >
+            <PinPin />
+            {is_pin_show && (
+                <PinContent to={link} anchor>
+                    <Text color="black" mr="8px">
+                        {title}
+                    </Text>
+                </PinContent>
+            )}
+        </PinWrapper>
+    )
+}
 
 const OurOffices = () => {
     const data = useStaticQuery(query)
 
     return (
         <StyledSectionContainer padding="0 16px 120px" background="var(--color-white)">
-            <StyledHeader as="h2" size="48px" align="center" type="page-title">
+            <StyledHeader as="h2" size="32px" align="center" type="page-title">
                 {localize('Our international offices')}
             </StyledHeader>
 
             <Flex>
-                <StyledQueryImage data={data['earth']} alt="example" width="100%" />
+                <MapImage data={data['earth']}>
+                    <MapPin
+                        left="30%"
+                        top="75%"
+                        title="Paraguay"
+                        link="/careers/locations/asuncion"
+                    />
+                    <MapPin
+                        left="50%"
+                        top="39.5%"
+                        title={localize('Malta')}
+                        link="/careers/locations/malta"
+                    />
+                    <MapPin
+                        left="55%"
+                        top="40%"
+                        title={localize('Cyprus')}
+                        link="/careers/locations/cyprus"
+                    />
+                    <MapPin
+                        left="54%"
+                        top="61.5%"
+                        title={localize('Rwanda')}
+                        link="/careers/locations/rwanda"
+                    />
+                    <MapPin
+                        left="54%"
+                        top="27.5%"
+                        title={localize('Belarus')}
+                        link="/careers/locations/minsk"
+                    />
+                    <MapPin
+                        left="61.5%"
+                        top="45.9%"
+                        title={localize('Dubai')}
+                        link="/careers/locations/dubai"
+                    />
+                    <MapPin
+                        left="73.8%"
+                        top="58.5%"
+                        title={localize('Ipoh')}
+                        link="/careers/locations/ipoh"
+                    />
+                    <MapPin
+                        left="74%"
+                        top="59%"
+                        title={localize('Cyberjaya')}
+                        link="/careers/locations/cyberjaya"
+                    />
+                    <MapPin
+                        left="74.2%"
+                        top="60%"
+                        title={localize('Melaka')}
+                        link="/careers/locations/melaka"
+                    />
+                    <MapPin
+                        left="77.2%"
+                        top="58.7%"
+                        title={localize('Labuan')}
+                        link="/careers/locations/labuan"
+                    />
+                    <MapPin left="45.8%" top="28%" title={localize('London')} link="" />
+                    <MapPin left="47.4%" top="30.9%" title={localize('Paris')} link="" />
+                    <MapPin left="44.5%" top="30.9%" title={localize('Guernsey')} link="" />
+                </MapImage>
             </Flex>
+
             <NumberSection columns="1fr 1fr 1fr 1fr" column_gap="120px" row_gap="4rem">
                 <Flex fd="column">
-                    <NumberHeader size="48px">{localize('750+')}</NumberHeader>
-                    <NumberText size="20px" align="left">
+                    <NumberHeader size="32px">{localize('750+')}</NumberHeader>
+                    <NumberText size="20px" align="center">
                         {localize('employees')}
                     </NumberText>
                 </Flex>
                 <Flex fd="column">
-                    <NumberHeader size="48px">{localize('50+')}</NumberHeader>
-                    <NumberText size="20px" align="left">
+                    <NumberHeader size="32px">{localize('50+')}</NumberHeader>
+                    <NumberText size="20px" align="center">
                         {localize('nationalities')}
                     </NumberText>
                 </Flex>
                 <Flex fd="column">
-                    <NumberHeader size="48px">{localize('13')}</NumberHeader>
-                    <NumberText size="20px" align="left">
+                    <NumberHeader size="32px">{localize('13')}</NumberHeader>
+                    <NumberText size="20px" align="center">
                         {localize('offices')}
                     </NumberText>
                 </Flex>
                 <Flex fd="column">
-                    <NumberHeader size="48px">{localize('10')}</NumberHeader>
-                    <NumberText size="20px" align="left">
+                    <NumberHeader size="32px">{localize('10')}</NumberHeader>
+                    <NumberText size="20px" align="center">
                         {localize('countries')}
                     </NumberText>
                 </Flex>
